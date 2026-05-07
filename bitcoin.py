dae691d9834ba01ea9f0c6b7fb7e37ed844f4c6a
vishnugettam <vishnugettam@gmail.com>
2026-05-07 16:32:05 +0530

    initial commit

---
+++ bitcoin.py
@@ -0,0 +1,13 @@
+from snowflake.snowpark import Session
+
+
+connection_params = {
+    "account": "<account_name>",
+    "user": "<username>",
+    "password": "<password>",   
+    "role": "<role_name>",
+    "warehouse": "<warehouse_name>",
+    "database": "<database_name>",
+    "schema": "<schema_name>"
+
+}

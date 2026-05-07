8a24593be88dc90910b68bef630109f87007c05b
vishnugettam <vishnugettam@gmail.com>
2026-05-07 18:34:50 +0530

    bitcoin snowpark code

--- bitcoin.py
+++ bitcoin.py
@@ -1,15 +1,16 @@
 from snowflake.snowpark import Session
 
-
 connection_params = {
-    "account": "<account_name>",
-    "user": "<username>",
-    "password": "<password>",   
-    "role": "<role_name>",
-    "warehouse": "<warehouse_name>",
-    "database": "<database_name>",
-    "schema": "<schema_name>"
-
+    "account": "IBWBDHN-WI07977",
+    "user": "vishnugettam",
+    "password": "Admin@123456789",
+    "role": "accountadmin",
+    "warehouse": "compute_wh",
+    "database": "bitcoin_dev",
+    "schema": "bitcoin_dev_schema_core",
 }
 
 snowflake_connection = Session.builder.configs(connection_params).create()
+
+df = snowflake_connection.table("tblbitcoin")
+df.show()
